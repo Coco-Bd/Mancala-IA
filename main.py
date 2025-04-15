83b5d8dcb10a4f96d4938fbd92e6f9d4b3bc91b1
rodove-tv <enzo.martinez.travail@gmail.com>
2025-04-15 09:58:56 +0200

    add main starter ui

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-"""import sys
+"""
+import sys
 import os
 
 # Ajouter le chemin du projet au PYTHONPATH
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Import correct depuis le dossier ui
-from ui.mancala_ui import MancalaUI
-from game.mancala import MancalaBoard
+from src.game.mancala import MancalaBoard
 
 def main():
     # Créer un jeu avec les dimensions par défaut
@@ -31,15 +31,19 @@ if __name__ == "__main__":
     main()
 """
 
+# main.py
 from src.ui.screen_menu import MenuScreen
+from db.init_db import initialize_files
 import pygame
 import sys
 
 def main():
     pygame.init()
+    initialize_files()
     menu = MenuScreen()
     result = menu.run()
     pygame.quit()
     sys.exit()
+
 if __name__ == "__main__":
     main()
